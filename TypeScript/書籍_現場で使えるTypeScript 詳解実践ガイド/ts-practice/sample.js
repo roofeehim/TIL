ca5c553525584fe832b578b6abd3536a75b607e5
roofeehim <roofeehim.txxt@gmail.com>
2024-05-05 18:35:22 +0900

    進捗更新

--- TypeScript/書籍_現場で使えるTypeScript 詳解実践ガイド/ts-practice/sample.js
+++ TypeScript/書籍_現場で使えるTypeScript 詳解実践ガイド/ts-practice/sample.js
@@ -1,1 +1,39 @@
 console.log("Hello,TypeScript");
+var firstName = "Bill";
+var age = 22;
+// リテラル型
+var adultAge;
+// ユニオン型とリテラル型
+var eventType;
+eventType = "click";
+var pet = "shibe";
+var book = {
+    title: "こころ",
+    author: "夏目漱石",
+    publishedIn: 1914,
+};
+var alice = {
+    name: "Alice",
+    age: 28,
+};
+var bob = {
+    name: "Bob"
+};
+var alice2 = {
+    name: "Alice",
+    age: 30,
+};
+alice2.age = 31;
+// 型注釈によるArray型の指定
+var fruits;
+fruits = ["Apple", "Grape", "Banana", "Peach", "Pear"];
+// 型注釈によるTuple型の指定
+var person = ["Alice", 30];
+var a = [1];
+var b = [1, "hello"];
+var c = [1, "hello", true, 10, "world"];
+var myCar = {
+    enginType: "V8",
+    volume: 3000,
+    wheelCount: 4
+};
--- TypeScript/書籍_現場で使えるTypeScript 詳解実践ガイド/ts-practice/sample.ts
+++ TypeScript/書籍_現場で使えるTypeScript 詳解実践ガイド/ts-practice/sample.ts
@@ -91,3 +91,132 @@ const myCar:Car = {
   volume:3000,
   wheelCount:4
 }
+
+// any型の変数の挙動
+let value1:any = 1;
+value1 = "noTypeCheck";
+value1.noCheck();
+
+let value2:any = [1,2,3];
+let value3 = value1 + value2;
+
+// unknown型の変数への代入
+let value4:unknown = 1;
+
+// NG
+// let value5:number = value4;
+
+// unknown型の変数の演算
+let value6:unknown = 1;
+
+// NG
+// console.log(value6 + 1);
+// unknown型の変数との+演算は許可されない
+
+// unknown型の変数を他の型へ特定
+// unknown型の変数に対して可能な操作は限られているが、比較演算は許可されている
+let value7:unknown = 10;
+if(typeof value7 === "number") {
+  console.log(value7 + 1);
+}
+
+// any型は型チェックを完全に放棄するが、unknown型は型の検証を要求する。これにより、型の安全性を維持しながら未知の型を扱うことが可能になる
+
+// 厳格なnullチェックによるエラー
+{
+  const person = {
+    age:25,
+    firstName:Math.random() > 5 ? "Alice" :null,
+  };
+
+  // NG. firstNameプロパティがnullである可能性があるためエラー
+  // console.log(person.firstName.toUpperCase()); 
+  // OK
+  console.log(person.firstName?.toUpperCase()); 
+}
+
+// 関数のパラメータ、戻り値の型注釈
+function addNumber(a:number,b:number):number {
+  return a + b;
+}
+
+// 関数のオプショナルパラメータ
+{
+  function printMessage(message?:string){
+    if(message){
+      console.log(message)
+    }else{
+      console.log("No message provided.");
+    }
+  }
+
+  printMessage("Hello world!");
+  printMessage();
+}
+
+// 関数型の構文
+{
+  let myFunction: (arg1:number,arg2:string) => boolean;
+}
+
+// 関数型と型エイリアス
+{
+  type AddFunction = (a:number,b:number) => number;
+  const addNumbers:AddFunction = (a,b) => a + b;
+}
+
+// 関数型の戻り値の型がvoid型の場合
+// 戻り値としてvoid型が指定された関数型の変数に、実際には値を返す関数を代入することが可能。その場合、戻り値の型情報は無視される
+{
+  type ReturnVoid = () => void;
+
+  // OK. 実際にはstring型の値を返す関数の代入
+  const greetWorld:ReturnVoid = () => {
+    return "Hello, World!";
+  }
+
+  // resultの型はvoidとして扱われ、関数の戻り値の型情報は無視される
+  const result = greetWorld(); // void型
+
+  // NG. TypeScriptは、resultの型をvoidと見なしているため、string型のメソッドは使えない
+  // console.log(result.toUpperCase());
+}
+
+// never型
+// void型が関数を何も返さないことを示すのに対し、never型は関数が正常に戻る「終了点」を持たないことを意味する
+// 無限ループや再起的な関数のように、終了しない関数
+// 例外をスローしてプログラムを異常終了させる関数
+// 到達不可能なコードのパスを示す場合
+{
+  function throwError(message:string):never {
+    throw new Error(message);
+  }
+
+  function infiniteLoop():never{
+    while(true){
+      console.log("infinite loop!");
+    }
+  }
+}
+
+// 関数オーバーロード
+{
+  // オーバーロードのシグネチャ
+  function addNumbers(a: number, b: number): number;
+  function addNumbers(a: string, b: string): string;
+  function addNumbers(a: number, b: string): string;
+  function addNumbers(a: string, b: number): string;
+
+  // 関数本体
+  function addNumbers(a: number | string, b: number | string): number | string {
+    if (typeof a === "number" && typeof b === "number") {
+      return a + b;
+    } else {
+      return a.toString() + b.toString();
+    }
+  }
+
+  let result = addNumbers("1", "2"); // result は string 型として推論される
+  // string 型と推論されているためエラーにならない
+  console.log(result.includes("1")); // true
+}
