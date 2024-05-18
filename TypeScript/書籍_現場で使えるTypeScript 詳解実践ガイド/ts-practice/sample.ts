8ba6fa9fcba77aa9a533462762d9a3ccbba5c9fa
roofeehim <roofeehim.txxt@gmail.com>
2024-05-18 16:29:45 +0900

    書籍_現場で使えるTypeScript 詳解実践ガイド_進捗

--- TypeScript/書籍_現場で使えるTypeScript 詳解実践ガイド/ts-practice/sample.ts
+++ TypeScript/書籍_現場で使えるTypeScript 詳解実践ガイド/ts-practice/sample.ts
@@ -220,3 +220,766 @@ function addNumber(a:number,b:number):number {
   // string 型と推論されているためエラーにならない
   console.log(result.includes("1")); // true
 }
+
+// インターフェイスの宣言
+{
+  interface Person {
+    name: string;
+    age: number;
+  }
+  
+  // 変数 john に Person インターフェイスを指定
+  let john: Person = {
+    name: "John",
+    age: 30,
+  };
+  
+  // NG
+  // john = "John";
+  // >> Type 'string' is not assignable to type 'Person'.
+  
+  // NG
+  // john = {
+  //   name: "John",
+  //   age: "30",
+  //   // >> Type 'string' is not assignable to type 'number'.
+  // };
+}
+
+// インターフェイスとメソッドの定義
+{
+  interface Person {
+    name: string;
+    age: number;
+    speak(word: string): void;
+  }
+  
+  const alice: Person = {
+    name: "Alice",
+    age: 25,
+    speak(word) {
+      console.log(word);
+    },
+  };
+  
+  alice.speak("Wonderful!!");
+}
+
+// インターフェイスとオプショナルプロパティ
+{
+  interface Person {
+    name: string;
+    age?: number;
+  }
+  
+  // ageプロパティを省略できる
+  const bob: Person = {
+    name: "Bob",
+  };
+}
+
+// インターフェイスと読み取り専用プロパティ
+{
+  interface Point {
+    readonly x: number;
+    readonly y: number;
+  }
+  
+  const point: Point = { x: 10, y: 20 };
+  
+  // NG. xは読み取り専用なので再代入は許可されない
+  // point.x = 30;
+  // >> Cannot assign to 'x' because it is a read-only property.
+}
+
+// stringインデックスシグネチャを持つ型の使用例
+{
+  interface FruitStock {
+    [i: string]: number;
+  }
+  
+  const fruit: FruitStock = {};
+  fruit.apple = 3;
+  fruit.orange = 5;
+  
+  // fruit.banana = "many"; // Error
+  // >> Type 'string' is not assignable to type 'number'.
+}
+
+// インデックスシグネチャと明示的なプロパティの混在
+{
+  interface FruitStock {
+    peach: number; // 明示的なプロパティ
+    [i: string]: number;
+  }
+  
+  const fruit: FruitStock = { peach: 1 }; // 明示的なプロパティは必須
+  fruit.apple = 3;
+  fruit.orange = 5;
+}
+
+// キー名がテンプレート文字列パターンのインデックスシグネチャ
+// インデックスシグネチャは、動的にプロパティ名が変わる可能性がある場合や、外部のデータソースを(APIの応答など)を扱う際に、プロパティ名が事前には定義されていないオブジェクトを型付けするのに適している
+{
+  interface Product {
+    [key: `product_${number}`]: string;
+  }
+  
+  // OK
+  const productA: Product = {
+    product_1: "foo",
+    product_2: "bar",
+    product_10: "baz",
+  };
+  
+  // NG
+  // const productB: Product = {
+  //   product_1: "foo",
+  //   product_2: "bar",
+  //   product_dx: "baz", // Error
+  // };
+  
+  // >> Object literal may only specify known properties, and 'product_dx' does not exist in type 'Product'.
+}
+
+// インターフェイスと呼び出しシグネチャ
+{
+  // 関数型による定義
+  type CalcFunction = (n1: number, n2: number) => number;
+
+  // インターフェイスによる定義
+  interface CalcInterface {
+    (n1: number, n2: number): number; // 呼び出しシグニチャ
+  }
+
+  const add: CalcFunction = (n1, n2) => n1 + n2;
+  const subtract: CalcInterface = (n1, n2) => n1 - n2;
+}
+
+// インターフェイスの拡張
+{
+  // 拡張元となるインターフェイス
+  interface Vehicle {
+    speed: number;
+  }
+
+  // Car は Vehicle を拡張したインターフェイス
+  interface Car extends Vehicle {
+    engineType: string;
+    volume: number;
+  }
+
+  // OK
+  const superCar: Car = {
+    speed: 240,
+    engineType: "V8",
+    volume: 4000,
+  };
+
+  // NG. 拡張元の Vehicle に存在するspeedプロパティが欠けているためエラー
+  // const sportsCar: Car = {
+  //   engineType: "V4",
+  //   volume: 3000,
+  // };
+  // >> Property 'speed' is missing in type '{ engineType: string; volume: number; }' but required in type 'Car'.
+}
+
+// プロパティのオーバーライド
+{
+  interface Vehicle {
+    speed: number;
+    model: string | null;
+  }
+  
+  interface Car extends Vehicle {
+    engineType: string;
+    model: string; // model プロパティをオーバーライド
+  }  
+}
+
+// 複数のインターフェイスの拡張
+{
+  interface Born {
+    birthYear: number;
+    place: string;
+  }
+  
+  interface Hobby {
+    hobbies: string[];
+  }
+  
+  // 複数のインターフェイスを拡張
+  interface Person extends Born, Hobby {
+    name: string;
+  }
+  
+  const mike: Person = {
+    name: "Mike",
+    birthYear: 1995,
+    place: "New York",
+    hobbies: ["tennis", "cooking", "chess"],
+  };  
+}
+
+// インターフェイスのマージ
+// 使用する機会が限られる機能に思われるが、外部ライブラリを拡張する際には有用
+{
+  interface Car {
+    engineType: string;
+    volume: number;
+  }
+  
+  // 2箇所で宣言したCarインターフェイスは自動的にマージされる
+  interface Car {
+    color: string;
+  }
+  
+  // OK. 過剰プロパティチェックが働くがエラーにはならない。Carインターフェイスがマージされていることがわかる
+  const myCar: Car = {
+    engineType: "V6",
+    volume: 3000,
+    color: "red",
+  };
+  
+  // NG. colorプロパティが欠けているためエラーになる。Carインターフェイスがマージされていることがわかる
+  // const herCar: Car = {
+  //   engineType: "V8",
+  //   volume: 4000,
+  // };
+  // >> Property 'color' is missing in type '{ engineType: string; volume: number; }' but required in type 'Car'.
+}
+
+// インターフェイスと型エイリアスの違い
+// オブジェクトの構造を定義する際にはインターフェイスが、複雑な型やユニオン型、インターセクション型などを扱う際には型エイリアスが使われることが一般的
+
+// クラスの宣言
+{
+  class Person {
+    name: string;
+    age: number;
+  
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+}
+
+// プロパティの宣言の省略によるエラー
+// TypeScriptでは、クラスのメンバーはトップレベルで明示的に宣言する必要がある
+// この点はJavaScriptのクラスの挙動と異なる
+{
+  // class Person {
+  //   constructor(name: string, age: number) {
+  //     this.name = name;
+  //     this.age = age;
+  //     // >> Property 'name' does not exist on type 'Person'.
+  //     // >> Property 'age' does not exist on type 'Person'.
+  //   }
+  // }
+}
+
+// プロパティの初期化チェック
+{
+  // class Person {
+  //   name: string;
+  //   age: number;
+  //   // >> Property 'age' has no initializer and is not definitely assigned in the constructor.
+  
+  //   constructor(name: string, age: number) {
+  //     this.name = name;
+  //   }
+  // }
+}
+
+// クラスのメソッドの追加
+{
+  class Person {
+    name: string;
+    age: number;
+  
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    greet(greeting: string): void {
+      console.log(
+        `${greeting}, my name is ${this.name} and I'm ${this.age} years old.`
+      );
+    }
+  }  
+}
+
+// オプショナルプロパティと読み取り専用プロパティ
+{
+  class Person {
+    readonly name: string; // 読み取り専用
+    age: number;
+    hobbies?: string[]; // オプショナル
+  
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    greet(greeting: string) {
+      console.log(
+        `${greeting}, my name is ${this.name} and I'm ${this.age} years old.`
+      );
+    }
+  }
+}
+
+// 型としてのクラス
+{
+  class Person {
+    name: string;
+    age: number;
+  
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+  
+  // 型としてクラスを使用
+  let john: Person;
+  john = new Person("John Doe", 25);
+  
+  // NG
+  // john = "John Doe";
+  // >> Type 'string' is not assignable to type 'Person'.
+}
+
+// オブジェクトの構造的型付け
+// オブジェクトがクラスに定義されているすべてのプロパティを持っている限り、そのオブジェクトをクラス型の変数に代入することが許可される
+{
+  class Person {
+    name: string;
+    age: number;
+  
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+  
+  // Personクラスを指定
+  let john: Person;
+  
+  // 型チェックOK
+  john = {
+    name: "John Doe",
+    age: 25,
+  };
+}
+
+// インスタンスを代入したときの変数の型
+{
+  class Person {
+    name: string;
+    age: number;
+  
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+  
+  // 変数 john の型は Person
+  const john = new Person("John Doe", 25);
+  
+  // NG
+  // john.gender = "male"; // Personクラスに存在しないプロパティにアクセスするとエラー
+  // >> Property 'gender' does not exist on type 'Person'.
+}
+
+// クラスの継承
+{
+  class Parent {
+    parentMethod() {
+      // スーパークラスのメソッド
+    }
+  }
+  
+  // Parentクラスを継承
+  class Child extends Parent {
+    childMethod() {
+      // サブクラスのメソッド
+    }
+  }
+  
+  const childObj = new Child();
+  childObj.parentMethod(); // 継承したスーパークラスのメソッドを呼び出せる
+  childObj.childMethod();  
+}
+
+// クラスの継承と型の互換性
+{
+  class Parent {
+    parentMethod() {
+      // スーパークラスのメソッド
+    }
+  }
+  
+  // Parentクラスを継承
+  class Child extends Parent {
+    childMethod() {
+      // サブクラスのメソッド
+    }
+  }
+  
+  // スーパークラスの型が要求されているが、サブクラスのインスタンスを代入可能
+  const objA: Parent = new Child();
+  
+  // NG
+  // const objB: Child = new Parent();
+  // >> Property 'childMethod' is missing in type 'Parent' but required in type 'Child'.
+}
+
+// クラスのプロパティのオーバーライド
+{
+  class SuperClass {
+    prop: number = 10;
+  }
+  
+  class SubClass extends SuperClass {
+    prop: number = 20; // SuperClassのプロパティをオーバーライド.
+  }  
+}
+
+// 型に互換性がないプロパティのオーバーライド
+{
+  class SuperClass {
+    prop: number = 10;
+  }
+  
+  // class SubClass extends SuperClass {
+  //   prop: string = "20"; // SuperClassのプロパティをオーバーライド.
+  // }
+  // >> Property 'prop' in type 'SubClass' is not assignable to the same property in base type 'SuperClass'.
+  // >> 　Type 'string' is not assignable to type 'number'.
+}
+
+// 型に互換性がないメソッドのオーバーライド
+{
+  class SuperClass {
+    method(value: number): string {
+      return `SuperClass method: ${value}`;
+    }
+  }
+  
+  // class SubClass extends SuperClass {
+  //   method(value: string): string {
+  //     return `SuperClass method: ${value}`;
+  //   }
+  // }
+  // >> Property 'method' in type 'SubClass' is not assignable to the same property in base type 'SuperClass'.
+  //  >> Type '(value: string) => string' is not assignable to type '(value: number) => string'.
+  //   >> Types of parameters 'value' and 'value' are incompatible.
+  //    >> Type 'number' is not assignable to type 'string'. 
+}
+
+// constructorのオーバーライド
+{
+  class Parent {
+    constructor(name: string) {
+      console.log(`Parent: ${name}`);
+    }
+  }
+  
+  class Child extends Parent {
+    constructor(name: string, age: number) {
+      super(name);
+      console.log(`Child: name => ${name}, age => ${age}`);
+    }
+  }
+  
+  const child = new Child("Child", 10);
+  // 実行結果
+  // Parent: Child
+  // Child: name => Child, age => 10  
+}
+
+// アクセス修飾子
+// public
+{
+  class Person {
+    public name: string; // 明示的にpublicと指定
+    age: number; // デフォルトでpublic
+  
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    public greet() {
+      console.log(
+        // 内部でnameとageにアクセス
+        `Hello, my name is ${this.name} and I'm ${this.age} years old.`
+      );
+    }
+  }
+  
+  const john = new Person("John", 25);
+  
+  // クラスの外部からアクセス
+  console.log(john.name); // John
+  console.log(john.age); // 25
+  john.greet(); // Hello, my name is John and I'm 25 years old.
+}
+
+// private
+{
+  class Person {
+    name: string;
+    private password: string;
+  
+    constructor(name: string, password: string) {
+      this.name = name;
+      this.password = password;
+    }
+  
+    public revealPassword() {
+      // OK. クラスの内部からはアクセス可能
+      console.log(`${this.name}'s password is: ${this.password}`);
+    }
+  }
+  
+  const jane = new Person("Jane", "12345");
+  
+  // NG. クラスの外部からはアクセスできない
+  // console.log(jane.password);
+  // >> Property 'password' is private and only accessible within class 'Person'.
+  
+  jane.revealPassword(); // Jane's password is: 12345
+}
+
+// protected
+{
+  class Person {
+    name: string;
+    protected saving: number;
+  
+    constructor(name: string, saving: number) {
+      this.name = name;
+      this.saving = saving;
+    }
+  }
+  
+  class Employee extends Person {
+    private salary: number;
+  
+    constructor(name: string, saving: number, salary: number) {
+      super(name, saving);
+      this.salary = salary;
+    }
+  
+    public introduce() {
+      console.log(
+        // サブクラスの内部ではsavingにアクセス可能
+        `I'm ${this.name}. My salary is ${this.salary} and my saving is ${this.saving}`
+      );
+    }
+  }
+  
+  const john = new Employee("John", 1000, 30);
+  
+  john.introduce();
+  // I'm John. My salary is 30 and my saving is 1000.
+  
+  // NG. savingはprotectedなのでクラスの外部からはアクセスできない
+  // console.log(john.saving);
+  // >> Property 'saving' is protected and only accessible within class 'Person' and its subclasses.
+}
+
+// アクセス修飾子と省略記法
+{
+  // ①これまでの書き方
+  // class Person {
+  //   name: string;
+  //   protected age: number;
+  //   private address: string;
+
+  //   constructor(name: string, age: number, address: string) {
+  //     this.name = name;
+  //     this.age = age;
+  //     this.address = address;
+  //   }
+
+  //   introduce() {
+  //     console.log(
+  //       `I'm ${this.name} and ${this.age} years old. I live at ${this.address}`
+  //     );
+  //   }
+  // }
+
+  // ②省略記法
+  class Person {
+    constructor(
+      // コンストラクタのパラメータにアクセス修飾子をつけることで、自動的にメンバー変数が宣言される
+      public name: string,
+      protected age: number,
+      private address: string
+    ) {}
+
+    introduce() {
+      console.log(
+        `I'm ${this.name} and ${this.age} years old. I live at ${this.address}`
+      );
+    }
+  }
+}
+
+// アクセサ（ゲッターとセッター）
+// ゲッターやセッターを使用すると、プロパティへの直接のアクセスを制限したり、プロパティのアクセス時に特定の処理を行うことができる
+{
+  class Circle {
+    private _radius: number;
+  
+    constructor(radius: number) {
+      this._radius = radius;
+    }
+  
+    // ゲッターの定義
+    get radius(): number {
+      console.log("半径を取得");
+      return this._radius;
+    }
+  
+    // セッターの定義
+    set radius(value: number) {
+      if (value <= 0) {
+        throw new Error("不正な値です。0より大きい値を入力してください。");
+      }
+      console.log("半径を設定");
+      this._radius = value;
+    }
+  }
+  
+  const circle = new Circle(3);
+  
+  // ゲッターの実行
+  console.log(circle.radius);
+  // ログ出力：
+  // >> 半径を取得
+  // >> 3
+  
+  // セッターの実行
+  circle.radius = 5;
+  // >> 半径を設定
+  
+  // NG. 負の値をセットしようとするとエラーが発生
+  circle.radius = -1;
+  // >> Error: 不正な値です。0より大きい値を入力してください。  
+}
+
+// staticプロパティ
+{
+  class Circle {
+    static PI: number = 3.14;
+    radius: number;
+  
+    constructor(radius: number) {
+      this.radius = radius;
+    }
+  
+    getArea(): number {
+      // メソッドからstaticプロパティのアクセスは可能
+      return Circle.PI * this.radius * this.radius;
+    }
+  }
+  
+  console.log(Circle.PI); // 3.14
+  
+  const circleA = new Circle(5);
+  console.log(circleA.getArea()); // 78.5
+  
+  const circleB = new Circle(10);
+  console.log(circleB.getArea()); // 314  
+}
+
+// 修飾子の組み合わせ
+// アクセス修飾子、static、readonlyの順序で記述する
+{
+  class Combination {
+    protected static readonly prop: string = "初期値";
+  
+    // 以下省略
+  }  
+}
+
+// staticプロパティはあ、インスタンス化することなくプロパティにアクセスできるため、メモリの使用効率が良い
+// すべてのインスタンスに影響を与える可能性があるため、使用する際には慎重に設計する必要がある
+
+// 抽象クラスと抽象メソッド
+// 抽象クラスは直接インスタンス化できない
+{
+  abstract class Animal {
+    // 抽象メソッドは、具体的な実装を持たせてはいけない
+    abstract makeSound(): void;
+  
+    move(): void {
+      console.log("The animal is moving.");
+    }
+  }
+}
+
+// サブクラスでの抽象メソッドの実装
+{
+  abstract class Animal {
+    // 抽象メソッドは、具体的な実装を持たせてはいけない
+    abstract makeSound(): void;
+  
+    move(): void {
+      console.log("The animal is moving.");
+    }
+  }
+  
+  class Dog extends Animal {
+    makeSound(): void {
+      console.log("Woof woof!");
+    }
+  }  
+}
+
+// インターフェイスの実装
+{
+  interface Shape {
+    getArea(): number;
+  }
+  
+  // CircleクラスはShapeインターフェイスを実装する
+  class Circle implements Shape {
+    radius: number;
+  
+    constructor(radius: number) {
+      this.radius = radius;
+    }
+  
+    getArea(): number {
+      return Math.PI * this.radius * this.radius;
+    }
+  }
+  
+  const circle = new Circle(5);
+  console.log(circle.getArea()); // 78.54 
+}
+
+// インターフェイスを正しく実装していない例
+{
+  interface Shape {
+    getArea(): number;
+  }
+  
+  // class Circle implements Shape {
+  //   radius: number;
+  
+  //   constructor(radius: number) {
+  //     this.radius = radius;
+  //   }
+  // }
+  // >> Class 'Circle' incorrectly implements interface 'Shape'.
+  //  >>  Property 'getArea' is missing in type 'Circle' but required in type 'Shape'.
+}
